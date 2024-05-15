233d667363a671441e16736ee1f66d580fc8c505
davidlotfi <boukernineabdellatif@gmail.com>
2024-05-15 23:30:02 +0200

    up

---
+++ Dashboard/boutique/success.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <title>success</title>
+    <style media="screen">
+
+    </style>
+  </head>
+  <body>
+      <div class="alert alert-success" role="alert">
+         <img src="icon-check.png" alt=""> La command fait avec  success
+      </div>
+      <a href="store.php"><button type="button" class="btn btn-primary btn-lg" style="position: absolute;
+      left: 100px;">new commande</button></a>
+
+      <a href="index.php"><button type="button" class="btn btn-primary btn-lg" style="position: absolute;
+      left: 400px;">Home</button></a>
+      <div class="container">
+           <div class="row">
+               <div class="heading text-center ">
+                    <img src="success.gif" alt="">
+               </div>
+           </div>
+      </div>
+  </body>
+</html>
